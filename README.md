7c96423f41d52578748855d1a197ff87d6c7270e
nakabonne <ryo@nakao.dev>
2021-03-14 14:59:57 +0900

    Make it possible to emit verbose debug

--- README.md
+++ README.md
@@ -1,10 +1,9 @@
 # rhack
-Would you like to quickly put a sneaky `dbg!` macro into external crates to find out how some internal data structure works? If so `rhack` is for you!
+Do you want to quickly put a sneaky macro kind of like `dbg!` into external crates to find out how some internal data structure works? If so `rhack` is for you!
 
 `rhack` makes it easier to edit external crates code that your project depends on.
 
 ## Usage
-
 Let's say you want to modify the `reqwest` crate.
 
 ```toml
@@ -18,14 +17,14 @@ Run the following:
 $ rhack edit reqwest
 ```
 
-This will make a copy of the crate into `$HOME/.rhack/reqwest-0.11.1` and add its path to the [[patch] section](https://doc.rust-lang.org/edition-guide/rust-2018/cargo-and-crates-io/replacing-dependencies-with-patch.html) in your Cargo.toml:
+This will make a copy of the crate into `$HOME/.rhack/reqwest-0.11.1` and add its path to the [[patch] section](https://doc.rust-lang.org/edition-guide/rust-2018/cargo-and-crates-io/replacing-dependencies-with-patch.html) in your Cargo.toml whose path is automatically detected:
 
 ```toml
 [patch.'https://github.com/nakabonne/rhack']
 reqwest = { path = "/home/you/.rhack/reqwest-0.11.1" }
 ```
 
-Now your package uses the locally checked out copy instead of one from crates.io. You can now put the `dbg!` macro or edit it as you like!
+Now your package uses the locally checked out copy instead of one from crates.io. You can now open the files (typically by jumping to definition) and then feel free to modify the source code.
 
 ### Undoing
 
--- src/cmd/edit.rs
+++ src/cmd/edit.rs
@@ -10,7 +10,7 @@ use anyhow::{anyhow, Result};
 use clap::Clap;
 use serde::Deserialize;
 use serde_json::Value;
-use toml_edit::{table, value, Document, Item, Table};
+use toml_edit::{table, value, Document};
 
 /// Start hacking a crate
 #[derive(Clap, Debug)]
@@ -25,7 +25,7 @@ pub struct Edit {
 impl Cmd for Edit {
     fn run(&self) -> Result<()> {
         // Determine the destination directory and the source directory.
-        let src = registry_path(&self.crate_name)?;
+        let src = self.crate_local_path()?;
         let mut dst = PathBuf::from(src.file_name().unwrap());
         match env::var(RHACK_DIR_ENV_KEY) {
             Ok(v) => {
@@ -40,7 +40,7 @@ impl Cmd for Edit {
             }
         };
 
-        match copy_dir(&src, &dst) {
+        match self.copy_dir(&src, &dst) {
             Ok(_) => (),
             Err(err) => return Err(anyhow!("failed to copy {:?} to {:?}: {}", src, dst, err)),
         }
@@ -52,6 +52,93 @@ impl Cmd for Edit {
 }
 
 impl Edit {
+    // Gives back the local path to the directory holding the given crate.
+    fn crate_local_path(&self) -> Result<PathBuf> {
+        #[derive(Deserialize)]
+        struct Metadata {
+            packages: Vec<Package>,
+        }
+
+        #[derive(Deserialize)]
+        struct Package {
+            name: String,
+            manifest_path: String,
+        }
+
+        let out = Command::new("cargo").arg("metadata").output();
+        let out = match out {
+            Ok(o) => o,
+            Err(err) => return Err(anyhow!("failed to run \"cargo metadata\": {:#}", err)),
+        };
+        let metadata: Metadata = serde_json::from_slice(&out.stdout)?;
+
+        let mut packages = HashMap::new();
+        for p in metadata.packages {
+            packages.insert(p.name, p.manifest_path);
+        }
+        let manifest_path = match packages.get(&self.crate_name) {
+            Some(m) => m,
+            None => return Err(anyhow!("the given crate is not used in this project")),
+        };
+        let manifest_path = PathBuf::from(manifest_path);
+        let path = manifest_path
+            .parent()
+            .expect("faild to determine the parent of manifest");
+        return Ok(path.to_path_buf());
+    }
+
+    // Copy the given src to the given dst recursively.
+    fn copy_dir<U: AsRef<Path>, V: AsRef<Path>>(
+        &self,
+        from: U,
+        to: V,
+    ) -> Result<(), std::io::Error> {
+        let mut stack = Vec::new();
+        stack.push(PathBuf::from(from.as_ref()));
+
+        let output_root = PathBuf::from(to.as_ref());
+        let input_root = PathBuf::from(from.as_ref()).components().count();
+
+        while let Some(working_path) = stack.pop() {
+            self.debug(&format!("\nprocess: {:?}", &working_path));
+
+            // Generate a relative path
+            let src: PathBuf = working_path.components().skip(input_root).collect();
+
+            // Create a destination if missing
+            let dest = if src.components().count() == 0 {
+                output_root.clone()
+            } else {
+                output_root.join(&src)
+            };
+            if fs::metadata(&dest).is_err() {
+                self.debug(&format!("  mkdir: {:?}", dest));
+                fs::create_dir_all(&dest)?;
+            }
+
+            for entry in fs::read_dir(working_path)? {
+                let entry = entry?;
+                let path = entry.path();
+                if path.is_dir() {
+                    stack.push(path);
+                } else {
+                    match path.file_name() {
+                        Some(filename) => {
+                            let dest_path = dest.join(filename);
+                            self.debug(&format!("    copy: {:?} -> {:?}", &path, &dest_path));
+                            fs::copy(&path, &dest_path)?;
+                        }
+                        None => {
+                            println!("failed to copy: {:?}", path);
+                        }
+                    }
+                }
+            }
+        }
+
+        Ok(())
+    }
+
     //  Update [patch.'https://github.com/nakabonne/rhack'] section in Cargo.toml
     fn update_manifest(&self, new_path: &PathBuf) -> Result<()> {
         // Run "cargo locate-project" to find out Cargo.toml file's location.
@@ -81,89 +168,10 @@ impl Edit {
 
         return Err(anyhow!("edit command is not implemented"));
     }
-}
 
-// Gives back the local path to the directory holding the given crate.
-fn registry_path(crate_name: &str) -> Result<PathBuf> {
-    #[derive(Deserialize)]
-    struct Metadata {
-        packages: Vec<Package>,
-    }
-
-    #[derive(Deserialize)]
-    struct Package {
-        name: String,
-        manifest_path: String,
-    }
-
-    let out = Command::new("cargo").arg("metadata").output();
-    let out = match out {
-        Ok(o) => o,
-        Err(err) => return Err(anyhow!("failed to run \"cargo metadata\": {:#}", err)),
-    };
-    let metadata: Metadata = serde_json::from_slice(&out.stdout)?;
-
-    let mut packages = HashMap::new();
-    for p in metadata.packages {
-        packages.insert(p.name, p.manifest_path);
-    }
-    let manifest_path = match packages.get(crate_name) {
-        Some(m) => m,
-        None => return Err(anyhow!("the given crate is not used in this project")),
-    };
-    let manifest_path = PathBuf::from(manifest_path);
-    let path = manifest_path
-        .parent()
-        .expect("faild to determine the parent of manifest");
-    return Ok(path.to_path_buf());
-}
-
-// Copy the given src to the given dst recursively.
-fn copy_dir<U: AsRef<Path>, V: AsRef<Path>>(from: U, to: V) -> Result<(), std::io::Error> {
-    let mut stack = Vec::new();
-    stack.push(PathBuf::from(from.as_ref()));
-
-    let output_root = PathBuf::from(to.as_ref());
-    let input_root = PathBuf::from(from.as_ref()).components().count();
-
-    while let Some(working_path) = stack.pop() {
-        // FIXME: emit only in verbose mode
-        println!("");
-        println!("process: {:?}", &working_path);
-
-        // Generate a relative path
-        let src: PathBuf = working_path.components().skip(input_root).collect();
-
-        // Create a destination if missing
-        let dest = if src.components().count() == 0 {
-            output_root.clone()
-        } else {
-            output_root.join(&src)
-        };
-        if fs::metadata(&dest).is_err() {
-            println!("  mkdir: {:?}", dest);
-            fs::create_dir_all(&dest)?;
-        }
-
-        for entry in fs::read_dir(working_path)? {
-            let entry = entry?;
-            let path = entry.path();
-            if path.is_dir() {
-                stack.push(path);
-            } else {
-                match path.file_name() {
-                    Some(filename) => {
-                        let dest_path = dest.join(filename);
-                        println!("    copy: {:?} -> {:?}", &path, &dest_path);
-                        fs::copy(&path, &dest_path)?;
-                    }
-                    None => {
-                        println!("failed: {:?}", path);
-                    }
-                }
-            }
+    fn debug(&self, msg: &str) {
+        if self.verbose {
+            println!("{}", msg);
         }
     }
-
-    Ok(())
 }
